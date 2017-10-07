30ca9871d6be977218463f943f569b02b9d9877a
Chathan Driehuys <chathan@driehuys.com>
2017-10-07 13:44:49 -0400

    Connect sensor info view to sensor data.

--- app/src/main/java/com/chathandriehuys/sensordisplay/SensorInfoView.java
+++ app/src/main/java/com/chathandriehuys/sensordisplay/SensorInfoView.java
@@ -2,11 +2,22 @@ package com.chathandriehuys.sensordisplay;
 
 import android.content.Context;
 import android.content.res.Resources;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 
+import java.util.Locale;
+
 
 public class SensorInfoView extends android.support.v7.widget.AppCompatTextView {
+    private static final String INFO_FORMAT =
+            "'%s' is present\n  - Range: %f\n  - Resolution: %f\n  - Delay: %d";
+
+    private Resources resources;
+
+    private Sensor sensor;
+
     public SensorInfoView(Context context) {
         super(context);
 
@@ -32,9 +43,29 @@ public class SensorInfoView extends android.support.v7.widget.AppCompatTextView 
         init();
     }
 
-    private void init() {
-        Resources r = getResources();
+    public void setSensorType(int sensorType) {
+        SensorManager manager = (SensorManager) getContext().getSystemService(Context.SENSOR_SERVICE);
+        sensor = manager.getDefaultSensor(sensorType);
 
-        setText(r.getString(R.string.loading_sensor_info));
+        if (sensor != null) {
+            setText(String.format(
+                    Locale.US,
+                    INFO_FORMAT,
+                    sensor.getName(),
+                    sensor.getMaximumRange(),
+                    sensor.getResolution(),
+                    sensor.getMinDelay()));
+        } else {
+            setText(resources.getString(R.string.sensor_info_not_found));
+        }
+    }
+
+    /**
+     * Initialize the view to display some loading text.
+     */
+    private void init() {
+        resources = getResources();
+
+        setText(resources.getString(R.string.sensor_info_loading));
     }
 }
